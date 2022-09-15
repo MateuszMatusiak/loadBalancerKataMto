c974faab5ae88aee9644a560d6760755f5aa4f82
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-09-15 13:18:43 +0200

    part 1

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,31 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+    private double expectedLoadPercentage;
+
+    public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    public static CurrentLoadPercentageMatcher hasCyrrentLoadOf(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
+
+    @Override
+    protected boolean matchesSafely(Server server) {
+        return doublesAreEqual(expectedLoadPercentage, server.currentLoadPercentage);
+    }
+
+    private boolean doublesAreEqual(double d1, double d2) {
+        return d1 == d2 || Math.abs(d1 - d2) < 0.01d;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a server with load percentage of ").appendValue(expectedLoadPercentage);
+    }
+
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -7,10 +7,33 @@ import static org.hamcrest.Matchers.equalTo;
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
+
+    @Test
+    public void balancingServerWithNoVms_serverStaysEmpty() {
+        Server theServer = a(ServerBuilder.server().withCapacity(1));
+        balancing(aServerListWith(theServer), anEmptyListOfVms());
+        assertThat(theServer, CurrentLoadPercentageMatcher.hasCyrrentLoadOf(0.0d));
+    }
+
+    private void balancing(Server[] aServerListWith, Vm[] anEmptyListOfVms) {
+
+    }
+
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
+
+    private Server[] aServerListWith(Server... servers) {
+        return servers;
+    }
+
+    private Server a(ServerBuilder builder) {
+        return builder.build();
+    }
 
 
 }
